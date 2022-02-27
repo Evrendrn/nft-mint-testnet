e4a0205b7e451961b4f86b37d1e6e6b9c0e1ace0
cryptomarla <100358642+cryptomarla@users.noreply.github.com>
2022-02-27 13:18:05 +0300

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -5,53 +5,76 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract NFT is ERC721URIStorage {
+contract NFTMARLAFREE is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    address public owner=0xCDeF3CC7cDBdC8695674973Ad015D9f2B01dD4C4;
-    uint public normalPrice =150000000000000000;
-    uint public totalMint=0;
-    uint public maxMint=1000;
-    string  public baseUrl="https://gateway.pinata.cloud/ipfs/QmToJQzZ8BpqZE4uVE8ZcRGemK3MUwbyjhHCGo7GpAtn3K/";
+    address public owner = 0xD4F1b63c2bb5D6eabE5729B347F5978e544A23B7;
+    uint256 public normalPrice = 50000000000000000;
+    uint256 public totalMint = 0;
+    uint256 public maxMint = 110;
+    string public baseUrl = "https://crypto-nfts.mypinata.cloud/ipfs/Qmck2EFdiLbYETDZr9sY261fEcb6t7tZ3CQnpGnAofPJ4Q/";
+    bool public mintingStatus = true;
 
+    constructor() ERC721("Marla NFTS", "MarlaNFT") {}
 
-    constructor() ERC721("Description", "NFT") {
-    }
-  
+    receive() external payable {}
 
-    receive() external payable {} 
-    
-     modifier onlyOwner{
-        require(
-            msg.sender==owner,"ADMIN_ONLY"
-        );
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ADMIN_ONLY");
         _;
     }
-    function append(string memory a, string memory b, string memory c) internal pure returns (string memory) {
 
-    return string(abi.encodePacked(a, b, c));
-
-}
-    function awardItem(address player, uint tokenCount) public payable{
-        require(totalMint<=maxMint);
-        require(normalPrice*tokenCount<=msg.value);
-        for (uint i=0; i<tokenCount; i++) {
-        uint256 newItemId = _tokenIds.current();
-        _tokenIds.increment();
-        _mint(player, newItemId);
-        _setTokenURI(newItemId, append(baseUrl,Strings.toString(newItemId+1),".json"));
-        totalMint++;
-        }
-
-
+    function append(
+        string memory a,
+        string memory b,
+        string memory c
+    ) internal pure returns (string memory) {
+        return string(abi.encodePacked(a, b, c));
     }
 
-    function claimBalance() external onlyOwner{
+    function mint(address player, uint256 tokenCount) public payable {
+        require(mintingStatus);
+        require(totalMint <= maxMint);
+        require(normalPrice * tokenCount <= msg.value);
+        for (uint256 i = 0; i < tokenCount; i++) {
+            uint256 newItemId = _tokenIds.current();
+            _tokenIds.increment();
+            _mint(player, newItemId);
+            _setTokenURI(
+                newItemId,
+                append(baseUrl, Strings.toString(newItemId + 1), ".json")
+            );
+            totalMint++;
+        }
+    }
+
+    function claimBalance() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
-    function changeOwner(address adres) external onlyOwner{
-        require(adres!=address(0x0));
-        owner=adres;
+
+    function changeStatus() external onlyOwner {
+        mintingStatus = !mintingStatus;
     }
 
+    function ownerAwardItem(address player, uint256 tokenCount)
+        public
+        payable
+        onlyOwner
+    {
+        for (uint256 i = 0; i < tokenCount; i++) {
+            uint256 newItemId = _tokenIds.current();
+            _tokenIds.increment();
+            _mint(player, newItemId);
+            _setTokenURI(
+                newItemId,
+                append(baseUrl, Strings.toString(newItemId + 1), ".json")
+            );
+            totalMint++;
+        }
+    }
+
+    function changeOwner(address adres) external onlyOwner {
+        require(adres != address(0x0));
+        owner = adres;
+    }
 }
