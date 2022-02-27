3fd3cdb48817ea88b8dc529a181f5574081d3951
cryptomarla <100358642+cryptomarla@users.noreply.github.com>
2022-02-27 11:33:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Moralis from "moralis"
 
 function App() {
-  const contractAddress="0x4664b4f76b6230D9CfbdA93ea73ee60b08827082";
+  const contractAddress="0x30182F5C701D0BBCc1C63683Efb5A9B5dd548F28";
   const rpcUrl="https://data-seed-prebsc-1-s1.binance.org:8545/"
   const nftPrice="0.15";
   const [mweb3,setMweb3]=useState();
